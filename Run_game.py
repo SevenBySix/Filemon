54d4ef87e7e460717de82f337412497ff037397c
SevenBySix <raidenqraidenq@gmail.com>
2022-09-14 19:48:43 -0400

    Player can now go into directories but now our (yet) Also major cleanup to how certain variables are used

--- Run_game.py
+++ Run_game.py
@@ -6,22 +6,24 @@ import World
 import Interface
 import os
 import time
-
+import Player
 
 def play_game(object):
-    startingPath = '''C:/Users'''
+    
     
     '''
     This is where the game will be initialized
     ultimately the player object will be initialized first, and the world will be created
     based on the location of the player
     '''
-    world = World.World(startingPath)
-    world.populateFilenames()
+    
+    
     #world.generateWorld(4)
-    playerPosition = 0
+    player = Player.Player('C:/Users', 0) #this should pull data from save as it is initialized
+    world = World.World(player.currentDir, player.position)
+    world.populateFilenames(player)
     isFinished = False
-    world.generateWorld(playerPosition)
+    world.generateWorld(player.position, player)
     
     inputLock = False
     inputUsed = False
@@ -32,15 +34,22 @@ def play_game(object):
         
         if(input == 's'):
 
-            playerPosition+=1 #to be replaced with something that updates the player objects position
-            world.generateWorld(playerPosition)
-                       
-        
+            player.position+=1 #to be replaced with something that updates the player objects position
             
+            world.generateWorld(player.position, player)
+        if(input == 'd'):
+            #go into directory at 
+            world.moveIntoDir(player)            
+            world.generateWorld(player.position, player)
+            
+            pass
+        if(input == 'a'):
+
+            pass
         
         if(input == 'w'):
-            playerPosition-=1
-            world.generateWorld(playerPosition)
+            player.position-=1
+            world.generateWorld(player.position, player)
             
         time.sleep(.3) #helps prevent unwanted input from holding down keypress
     pass
