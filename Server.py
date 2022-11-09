f548d84da5e605a4bc0e88f8383630dbda4a3feb
hunter <hf5@email.sc.edu>
2022-11-08 19:00:54 -0500

    allows a client to connect to the server as well as gives them a client id

--- Server.py
+++ Server.py
@@ -1,52 +1,53 @@
 import socket
 from _thread import *
 import sys
-import os
-import Player
-
-
-server = "192.168.1.82"
-port = 5556
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
+server = '192.168.1.200'
+port = 5555
+
+server_ip = socket.gethostbyname(server)
+
 try:
-	s.bind((server, port))
+    s.bind((server, port))
+
 except socket.error as e:
+    print(str(e))
 
-	str(e)
+s.listen(10)
+print("Waiting for a connection")
 
-	
-	
-
-s.listen(2)
-print("Waiting for a connection, Server Started")
+currentId = "0"
 
 def threaded_client(conn):
-	
-	replay = ""
-	while True:
-		try:
-			data = conn.recv(2048)
-			reply = data.decode("utf-8")
+    global currentId
+    conn.send(str.encode(currentId))
+    currentId = "0"
+    reply = ''
+    while True:
+        try:
+            data = conn.recv(2048)
+            reply = data.decode('utf-8')
+            if not data:
+                conn.send(str.encode("Goodbye"))
+                break
+            else:
+                print("Recieved: " + reply)
+                arr = reply.split(":")
 
-			if not data:
-				print("Disconnected")
-				break
-			else:
-				print("Received: ", reply)
-				print("Sending : ", reply)
+            conn.sendall(str.encode(reply))
+        except:
+            break
 
-			conn.sendall(str.encode(reply))
-		except:
-			break
+    print("Connection Closed")
+    conn.close()
 
-while True: 
-	conn, addr = s.accept()
-	print("Connected to:", addr)
+while True:
+    conn, addr = s.accept()
+    print("Connected to: ", addr)
 
-	start_new_thread(threaded_client, (conn,))
-
+    start_new_thread(threaded_client, (conn,))
 
 
 
