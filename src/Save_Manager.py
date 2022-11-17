57adf8309bb323a0cea5bc6ce299a6d039485026
SevenBySix <raidenqraidenq@gmail.com>
2022-11-17 15:15:04 -0500

    fixed saving

--- src/Save_Manager.py
+++ src/Save_Manager.py
@@ -6,27 +6,26 @@ stored in a folder called "Saves".
 import pickle
 #import pandas as pd
 import src.Player as Player
-class Save_Manager(object):
-    
-    def __init__(self):
-        
-        pass
 
-    #Saves player object to pickle file in the saves folder
-    def saveGame(playerObj):
-        with open('GameSaves/save.pkl', "wb") as savefile:
-            pickle.dump(playerObj, savefile)
-            
-        savefile.close()
+def __init__(self):
         
-    #Loads player object from pickle file and returns player object 
-    def loadGame():
-        
-        with open('GameSaves/save.pkl', "rb") as savefile:
-            playerObj = pickle.load(savefile)
+    pass
+
+#Saves player object to pickle file in the saves folder
+def saveGame(playerObj):
+    with open('GameSaves/save.pkl', "wb") as savefile:
+        pickle.dump(playerObj, savefile)
             
-        savefile.close()
-        return playerObj  
+    savefile.close()
+        
+#Loads player object from pickle file and returns player object 
+def loadGame():
+        
+    with open('GameSaves/save.pkl', "rb") as savefile:
+        playerObj = pickle.load(savefile)
+            
+    savefile.close()
+    return playerObj  
    
 
 
